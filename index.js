603bd58377f3af97b877492aacc8e4b9abef76cf
Marek Czernek <mczernek@redhat.com>
2018-10-15 21:05:07 +0200

    Deadend with mysql

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const xmlparser = require('express-xml-bodyparser')
 const mysql = require('mysql')
 const con = mysql.createConnection({
   host: "localhost",
-  user: "process.env.USER",
-  password: "process.env.PASSWORD",
-  database: "process.env.DB"
-  // insecureAuth : true
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
+  insecureAuth : true
 });
 
 const app = express()
@@ -44,12 +44,21 @@ app.post('/api/post/parsexml', (request, response) => {
   const tests = request.body.testsuite.testcase
 
   // Get all fails; filter all else
-  const fails = tests.filter(function(test) {
+  const failedTests = tests.filter(function(test) {
     return typeof test.failure != "undefined"
-  }).map(failedTest => failedTest.failure)
+  })
 
-  console.log("found fails:", fails.length)
-  console.log(fails[1][0]['$'])
+  console.log("found fails:", failedTests.length)
+
+  failedTests.forEach(failedTest => {
+    const testProperties = failedTest['$']
+    saveToDatabase({classname:testProperties.classname, 
+      testname: testProperties.name, 
+      failMsg: failedTest.failure[0]['$'].message})
+  });
+
+  //Is CLASS in DB? If so, get ID; else insert and get ID
+
 
   // con.connect(function(err) {
   //   if (err) throw err;
@@ -62,5 +71,33 @@ app.post('/api/post/parsexml', (request, response) => {
   // });
   response.sendStatus(200)
 })
+/**
+ * Saves parsed fails into the DB.
+ * // TODO: MongoDB 
+ * @param {*} failInfo 
+ */
+function saveToDatabase(failInfo) {
+  console.log(failInfo.failMsg)
+  console.log(failInfo.classname, failInfo.testname)
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var selectSql = "SELECT ID FROM CLASS WHERE FQCN=?;"
+    con.query(selectSql, failInfo.classname, function (err, result, fields) {
+      if (err) throw err;
+      console.log("FIRST!", result[0]);
+    })
+
+    con.query(selectSql, "com.ex", function (err, result, fields) {
+      if (err) throw err;
+      console.log("SECOND!", result[0].ID);
+    })
+    // var insertSql = "INSERT IGNORE INTO CLASS (FQCN) VALUES ?";
+
+    // con.query(insertSql, failInfo.classname, function (err, result) {
+    //   if (err) throw err;
+    // });
+
+  })}
 
 app.listen(3000)
