992bf76fffeec18369d495c3f3955102374a5054
Marek Czernek <mczernek@redhat.com>
2018-10-26 12:32:18 +0200

    Polish frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,20 @@
+var hide = true;
+
 $(document).click(function(event) {
     const _class = $(event.target).attr('class');
     if(_class !== undefined && _class.includes('stacktrace')) {
         $(event.target).parent().parent().next('tr').toggle();
     }
+    
+    if(_class !== undefined && _class.includes('js_triggerable')) {
+        const _trParrent = $(event.target).parent().parent();
+        if(hide) {
+            $(_trParrent).nextUntil('.warning').css('display', 'none');
+            hide = !hide;
+        } else {
+            $(_trParrent).nextUntil('.warning').css('display', 'table-row')
+            hide = !hide;
+        }
+    }
+
 });
